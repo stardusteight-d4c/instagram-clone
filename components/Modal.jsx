086e6d98f6f57e01a2e26f76e0207327832e479d
Gabriel Sena <stardusteight.d4cc@gmail.com>
2022-07-29 18:13:07 -0300

    useEffect and setPosts with onSnapshot query

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -154,11 +154,13 @@ const Modal = () => {
               </div>
 
               <div className="flex items-center justify-center mt-5 sm:mt-6">
-                <button>
+                <button onClick={uploadPost} disabled={!selectedFile}>
                   <a className="relative cursor-pointer p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-md">
                     <span className="w-full h-full bg-gradient-to-br from-[#ff8a05] via-[#ff5478] to-[#ff00c6] group-hover:from-[#ff00c6] group-hover:via-[#ff5478] group-hover:to-[#ff8a05] absolute"></span>
                     <span className="relative px-6 py-3 transition-all ease-out bg-gray-900 rounded-md group-hover:bg-opacity-0 duration-400">
-                      <span className="relative text-white">Fazer Upload</span>
+                      <span className="relative text-white">
+                        {loading ? 'Enviando...' : 'Enviar post'}
+                      </span>
                     </span>
                   </a>
                 </button>
--- components/Posts.jsx
+++ components/Posts.jsx
@@ -1,34 +1,33 @@
-import React from 'react'
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
+import React, { useEffect, useState } from 'react'
+import { db } from '../firebase'
 import Post from './Post'
 
-const posts = [
-  {
-    id: '123',
-    username: 'stardusteight-d4c',
-    userImg: 'https://avatars.githubusercontent.com/u/87643260?v=4',
-    img: 'https://avatars.githubusercontent.com/u/87643260?v=4',
-    caption: 'Hello people from the World!',
-  },
-  {
-    id: '124',
-    username: 'stardusteight-d4c',
-    userImg: 'https://avatars.githubusercontent.com/u/87643260?v=4',
-    img: 'https://avatars.githubusercontent.com/u/87643260?v=4',
-    caption: 'Hello people from the World!',
-  },
-]
-
 const Posts = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, 'posts'), orderBy('timestamp', 'desc')),
+        (snapshot) => {
+          setPosts(snapshot.docs)
+        }
+      ),
+    []
+  )
+
+  // console.log(posts[0]._document.data.value.mapValue.fields)
   return (
     <div>
       {posts.map((post) => (
         <Post
           key={post.id}
           id={post.id}
-          username={post.username}
-          userImg={post.userImg}
-          img={post.img}
-          caption={post.caption}
+          username={post.data().username}
+          userImg={post.data().profileImg}
+          img={post.data().image}
+          caption={post.data().caption}
         />
       ))}
     </div>
