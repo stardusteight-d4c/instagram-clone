edf80baf6e1fb20e0b338b4a61b3b67556f4e4c8
Gabriel Sena <stardusteight.d4cc@gmail.com>
2022-07-30 05:21:12 -0300

    finalizando projeto base

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,5 +1,19 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSession } from 'next-auth/react'
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  onSnapshot,
+  orderBy,
+  query,
+  setDoc,
+  doc,
+  deleteDoc,
+} from '@firebase/firestore'
+import { db } from '../firebase'
+import Moment from 'react-moment'
 import {
   BookmarkIcon,
   ChatIcon,
@@ -11,6 +25,64 @@ import {
 import { HeartIcon as HeartIconSolid } from '@heroicons/react/solid'
 
 const Post = ({ id, username, userImg, img, caption }) => {
+  const { data: session } = useSession()
+  const [comment, setComment] = useState('')
+  const [comments, setComments] = useState([])
+  const [likes, setLikes] = useState([])
+  const [hasLiked, setHasLiked] = useState(false)
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(
+          collection(db, 'posts', id, 'comments'),
+          orderBy('timestamp', 'desc')
+        ),
+        (snapshot) => setComments(snapshot.docs)
+      ),
+    [id]
+  )
+
+  useEffect(
+    () =>
+      onSnapshot(collection(db, 'posts', id, 'likes'), (snapshot) =>
+        setLikes(snapshot.docs)
+      ),
+    [id]
+  )
+
+  useEffect(
+    () =>
+      setHasLiked(
+        likes.findIndex((like) => like.id === session?.user?.uid) !== -1
+      ),
+    [likes, session?.user?.uid]
+  )
+
+  const likePost = async () => {
+    if (hasLiked) {
+      await deleteDoc(doc(db, 'posts', id, 'likes', session.user.uid))
+    } else {
+      await setDoc(doc(db, 'posts', id, 'likes', session.user.uid), {
+        username: session.user.username,
+      })
+    }
+  }
+
+  const sendComment = async (e) => {
+    e.preventDefault()
+
+    const commentToSend = comment
+    setComment('')
+
+    await addDoc(collection(db, 'posts', id, 'comments'), {
+      comment: commentToSend,
+      username: session.user.username,
+      userImage: session.user.image,
+      timestamp: serverTimestamp(),
+    })
+  }
+
   return (
     <div className="bg-white border rounded-sm my-7">
       {/* Header */}
@@ -34,33 +106,74 @@ const Post = ({ id, username, userImg, img, caption }) => {
       />
 
       {/* Buttons */}
-      <div className="flex justify-between px-4 pt-4">
-        <div className="flex space-x-4">
-          <HeartIcon className="btn" />
-          <ChatIcon className="btn" />
-          <PaperAirplaneIcon className="-mt-1 rotate-45 btn" />
+      {session && (
+        <div className="flex justify-between px-4 pt-4">
+          <div className="flex space-x-4">
+            {hasLiked ? (
+              <HeartIconSolid onClick={likePost} className="text-red-500 btn" />
+            ) : (
+              <HeartIcon onClick={likePost} className="btn" />
+            )}
+            <ChatIcon className="btn" />
+            <PaperAirplaneIcon className="-mt-1 rotate-45 btn" />
+          </div>
+          <BookmarkIcon className="btn" />
         </div>
-        <BookmarkIcon className="btn" />
-      </div>
+      )}
 
       {/* caption */}
       <p className="p-5 truncate">
+        {likes.length > 0 && (
+          <p className="mb-1 font-bold">{likes.length} likes</p>
+        )}
+
         <span className="mr-1 font-bold">{username}</span>
         {caption}
       </p>
 
       {/* comments */}
+      {comments.length > 0 && (
+        <div className="h-20 ml-10 overflow-y-scroll scrollbar-thumb-black scrollbar-thin">
+          {comments.map((comment) => (
+            <div key={comment.id} className="flex items-center mb-3 space-x-2">
+              <img
+                src={comment.data().userImage}
+                alt=""
+                className="rounded-full w-7"
+              />
+              <p className="flex-1 text-sm">
+                <span className="font-bold">{comment.data().username}</span>{' '}
+                {comment.data().comment}
+              </p>
+              <Moment fromNow className="pr-5 text-xs">
+                {comment.data().timestamp?.toDate()}
+              </Moment>
+            </div>
+          ))}
+        </div>
+      )}
 
       {/* input box */}
-      <form className="flex items-center p-4">
-        <EmojiHappyIcon className="w-7" />
-        <input
-          type="text"
-          placeholder="Deixe um comentário..."
-          className="flex-1 border-none outline-none focus:ring-0"
-        />
-        <button className="font-semibold text-blue-400">Post</button>
-      </form>
+      {session && (
+        <form className="flex items-center p-4">
+          <EmojiHappyIcon className="w-7" />
+          <input
+            type="text"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            placeholder="Deixe um comentário..."
+            className="flex-1 border-none outline-none focus:ring-0"
+          />
+          <button
+            type="submit"
+            disabled={!comment.trim()}
+            onClick={sendComment}
+            className="font-semibold text-blue-400 disabled:cursor-not-allowed"
+          >
+            Post
+          </button>
+        </form>
+      )}
     </div>
   )
 }
